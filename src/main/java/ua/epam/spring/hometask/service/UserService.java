79fb99c1beb4bf13f3ec0ce36827c25b42c5c840
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2016-04-11 09:50:19 +0300

    realize interfaces EventService and UserService

--- src/main/java/ua/epam/spring/hometask/service/UserService.java
+++ src/main/java/ua/epam/spring/hometask/service/UserService.java
@@ -1,12 +1,13 @@
 package ua.epam.spring.hometask.service;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
 import ua.epam.spring.hometask.domain.User;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Set;
+
 /**
- * @author Yuriy_Tkach
+ * @author Maksym_Mazurkevych
  */
 public interface UserService extends AbstractDomainObjectService<User> {
 
@@ -18,5 +19,31 @@ public interface UserService extends AbstractDomainObjectService<User> {
      * @return found user or <code>null</code>
      */
     public @Nullable User getUserByEmail(@Nonnull String email);
+
+    /**
+     *
+     * @param id
+     *          Id of the user
+     * @return Finding user by Id
+     */
+    public @Nullable User getById(@Nonnull String id);
+
+    /**
+     *
+     * @param users
+     *          Finding all users
+     * @return set of all users
+     */
+    public @Nullable Set<User> getAll(User users);
+
+    /**
+     * Saves user
+     */
+    public void save();
+
+    /**
+     * Removes user
+     */
+    public void remove();
 
 }
