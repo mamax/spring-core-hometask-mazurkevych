837c0259304d0033e119ce5b358cdddea4af8d26
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2016-04-25 18:54:13 +0300

    add discountStrategy for calculating discounts

--- src/main/java/ua/epam/spring/hometask/domain/DiscountStrategy.java
+++ src/main/java/ua/epam/spring/hometask/domain/DiscountStrategy.java
@@ -1,7 +1,14 @@
 package ua.epam.spring.hometask.domain;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.time.LocalDateTime;
+
 /**
  * Created by Maksym_Mazurkevych on 4/22/2016.
  */
 public interface DiscountStrategy {
+
+    byte getDiscount(@Nullable User user, @Nonnull Event event, @Nonnull LocalDateTime airDateTime, long numberOfTickets);
+
 }
